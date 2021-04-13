4c1473bd6785c1131f5c4ecff8c601c2977ba625
claudio-code <suporte2424@outlook.com>
2021-04-13 00:08:57 -0300

    :star: created repository

---
+++ application/repositories/job_repository.go
@@ -0,0 +1,46 @@
+package repositories
+
+import (
+	"encoder/domain"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+)
+
+type JobRepository interface {
+	Insert(job *domain.Job) (*domain.Job, error)
+	Find(id string) (*domain.Job, error)
+}
+
+type JobRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func NewJobRepository(db *gorm.DB) *JobRepositoryDb {
+	return &JobRepositoryDb{Db: db}
+}
+
+func (repo *JobRepositoryDb) Insert(job *domain.Job) (*domain.Job, error) {
+	if job.ID == "" {
+		job.ID = uuid.NewV4().String()
+	}
+
+	err := repo.Db.Create(job).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (repo *JobRepositoryDb) Find(id string) (*domain.Job, error) {
+	var job domain.Job
+	repo.Db.Find(&job, "id = ?", id)
+
+	if job.ID == "" {
+		return nil, fmt.Errorf("job does not found in database.")
+	}
+
+	return &job, nil
+}
